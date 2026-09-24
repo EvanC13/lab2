b57e7acefe8b9fca1562b0b91da888be426dfa5a
Evan Clark <evanc413@outlook.com>
2026-09-24 18:09:28 -0400

    First steps!

---
+++ tripCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿//Part 1:
+
+//Input for Part 1
+System.Console.Write("How many miles for your trip? ");
+double tripMiles = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("How many miles per gallon can your car go? ");
+double milesPerGallon = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("How much is one gallon of gas? ");
+double pricePerGallon = Convert.ToDouble(Console.ReadLine());
+
+//Calculations for Part 1
+
+double gallonsNeeded = tripMiles / milesPerGallon;
+double fuelCost = gallonsNeeded * pricePerGallon;
+
+//Print Calculations
+System.Console.WriteLine("Gallons needed: " + gallonsNeeded.ToString("F2"));
+System.Console.WriteLine("Fuel cost: " + fuelCost.ToString("C"));
